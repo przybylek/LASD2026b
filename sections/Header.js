04cdbeacd982babd9f3d4d21051e8e56edc8488e
Adam Przybylek <adam.przybylek@gmail.com>
2025-12-15 21:28:25 +0100

    UTF8 fix

--- sections/Header.js
+++ sections/Header.js
@@ -14,7 +14,7 @@ const Header = () => (
           <h1 className='h1 text-center anchor font-weight-bold'>LASD@<a className='u' href='https://isd2026.ksi.fit.cvut.cz/'>ISD'26</a></h1>
           <h2 className='text-center h3 py-3'>Track on <strong>Lean and Agile Software Development</strong></h2>
           <h3 className='text-center h4 pb-3'><small>at the 34rd International Conference on Information Systems Development</small></h3>
-          <h4 className='text-center h5'>September 2–4, 2026</h4>
+          <h4 className='text-center h5'>September 2â€“4, 2026</h4>
           <h4 className='text-center h5 pb-3'>Prague, Czechia</h4>
         </div>
       </div>
